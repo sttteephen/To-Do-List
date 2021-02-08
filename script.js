a38f30c5e768ce1cee4704a1667cf717f0e74826
Stephen Messer <40314216@live.napier.ac.uk>
2021-02-08 16:35:19 +0000

    added save button

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var taskCount = 1;
-
+var taskCount = 0;
 
 // adds a new task div when + button is pressed
 function addTask() {
-    taskCount++;
     
     // create new div, add it to the class and give it an id
     var newTaskDiv = document.createElement("div");
@@ -37,6 +35,8 @@ function addTask() {
     // add function call to new checkbox
     newCheck.addEventListener('click', function() {checkTask(this)}, false);
     newTaskDiv.appendChild(newCheck);
+
+    taskCount++;
 }
 
 
@@ -52,7 +52,7 @@ function checkTask(element) {
 
     var newParent;
     
-    // decide where to move task div based on state of checkbox
+    // decide where to move task div and change visibiliy of cross button based on state of checkbox 
     if(document.getElementById(`check${taskNo}`).checked == true) {
         newParent = document.getElementById("completedSection");
         crossBtn.style.visibility = "visible";
@@ -68,8 +68,27 @@ function deleteTask(element) {
     var taskNo = element.id;
     taskNo = taskNo.substr(5);
 
-    // access the relevant task div using the task number
+    // get the relevant task div using the task number and remove from screen
     var taskDiv = document.getElementById("taskdiv" + taskNo);
-
     taskDiv.remove();
+}
+
+
+function saveTasks() {
+    localStorage.clear();
+    var tasks = document.getElementsByClassName("task");
+
+    for(let i = 0; i < tasks.length; i++) {
+        var taskDiv = tasks[i];
+        var taskIdNo = taskDiv.id.substr(7);
+
+        var taskText = document.getElementById("task" + taskIdNo);
+        var taskCheck = document.getElementById("check" + taskIdNo);  
+        
+        if(taskCheck.checked == true) {
+            localStorage.setItem(taskText.value, "completed")
+        } else {
+            localStorage.setItem(taskText.value, "uncompleted")
+        }
+    }
 }
